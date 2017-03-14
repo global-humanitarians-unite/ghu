df9b939fb95382db3a95c2312c04a068e624500e
Austin Adams <git@austinjadams.com>
2017-03-14 18:42:26 -0400

    Fill orgs page with sample data
    And style it nicely.

--- uh_web/uh_browser/views.py
+++ uh_web/uh_browser/views.py
@@ -7,7 +7,15 @@ def about(request):
     return render(request, 'uh_browser/about.html', {})
 
 def orgs(request):
-    return render(request, 'uh_browser/orgs.html', {})
+    # XXX remove this
+    from collections import namedtuple
+    Org = namedtuple('Org', ['name', 'loc', 'categs'])
+
+    ctx = {
+        'results': [Org('An Organization', 'Atlanta, GA, United States', ['health', 'seniors', 'education'])]*64,
+    }
+
+    return render(request, 'uh_browser/orgs.html', ctx)
 
 def forum(request):
     return render(request, 'uh_browser/forum.html', {})
