bc93132a50f671ada00c8a50fcca733349ffcd4d
Schuyler Reinken <sreinken3@gatech.edu>
2017-05-07 13:07:41 -0400

    Updated toolkits and structure for testing
    Added toolkits/ to urls in hu_web among other things.

--- hu_web/hu_web/settings.py
+++ hu_web/hu_web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hu',
     'hu_browser',
+    'ghu-toolkits',
 ]
 
 MIDDLEWARE = [
--- hu_web/hu_web/urls.py
+++ hu_web/hu_web/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('hu_browser.urls')),
+    url(r'^', include('ghu-toolkits.urls')),
     url(r'^admin/', admin.site.urls),
 ]
