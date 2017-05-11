2fe015c78d3bea5a392810269b658021cde874e4
Austin Adams <git@austinjadams.com>
2017-05-11 18:37:47 -0400

    Delete ghu app
    Since ghu_global serves the purpose for which I originally envisioned
    the ghu app, holding global models, delete the ghu app from the project.

--- ghu_web/ghu_web/settings.py
+++ ghu_web/ghu_web/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ghu',
     'ghu_global',
     'ghu_main',
     'ghu_toolkits',
