f9d9c305c0935103299fea5b77ba96b04087a443
Austin Adams <git@austinjadams.com>
2017-05-14 17:46:21 -0400

    For admin, specify verbose app name
    Specify a verbose app name of "GHU (Toolkits)," which will show up in
    the admin instead of "Ghu_toolkits."

---
+++ __init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'ghu_toolkits.apps.GhuToolkitsConfig'
--- apps.py
+++ apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class GhuToolkitsConfig(AppConfig):
     name = 'ghu_toolkits'
+    verbose_name = 'GHU (Toolkits)'
