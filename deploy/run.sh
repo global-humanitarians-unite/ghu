a99d9166a17378db3f80779624d807b5f00129b6
Austin Adams <git@austinjadams.com>
2017-09-03 11:21:40 -0400

    run.sh: Rework for new deploy setup
    Make run.sh use deploy.config to know where to place sockets etc. and
    which virtualenv to use.

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-exec uwsgi --module ghu_web.wsgi:application -s sock --umask 0
+
+[[ -z $GHU_DEPLOY_CONFIG_PATH ]] && {
+    printf 'error: $GHU_DEPLOY_CONFIG_PATH is not set. Set it to the path to '`
+          `'deploy.config and try again\n' >&2
+    exit 1
+}
+
+. "$GHU_DEPLOY_CONFIG_PATH"
+
+venv="$deploy_root/venv"
+worktree="$deploy_root/worktrees/current"
+
+exec "$venv/bin/uwsgi" --virtualenv "$venv" \
+                       --module ghu_web.wsgi:application \
+                       --pythonpath "$worktree/ghu_web" \
+                       --socket "$deploy_root/uwsgi.sock" \
+                       --umask 0
