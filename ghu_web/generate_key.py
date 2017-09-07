bf315e293c553ad4b1dfb81d192ab312d44c548e
Austin Adams <git@austinjadams.com>
2017-09-07 09:35:31 -0400

    generate_key: Move code into main(), follow pylint
    So that you can safely import this module without executing a bunch of
    stuff, move the generation code into main(), executing it only when the
    module is called directly.

--- ghu_web/generate_key.py
+++ ghu_web/generate_key.py
@@ -13,35 +13,46 @@ import os
 import os.path
 from shutil import copyfile
 
-population = string.ascii_letters + string.punctuation + string.digits
-key_len = 512
-
-# If user supplies first argument, use it as the path to the config.ini to
-# write to, else use 'config.ini' in the current directory
-if len(sys.argv) - 1 >= 1:
-    config_ini_path = sys.argv[1]
-else:
-    config_ini_path = 'config.ini'
-print("Writing secret key to "
-      "ini file `{}'...".format(os.path.abspath(config_ini_path)))
-
-print('Generating key...')
-random_bytes = os.urandom(key_len)
-# Work through the random data from /dev/urandom byte by byte, converting it to
-# characters from population
-key = ''.join(population[int(x / 256 * len(population))] for x in random_bytes)
-secrets_section = """
+POPULATION = string.ascii_letters + string.punctuation + string.digits
+KEY_LEN = 512
+SECRETS_SECTION = """
 [secrets]
 secret_key = {}
-""".format(key)
+"""
 
-if not os.path.exists(config_ini_path):
-    print("`{}' does not exist, so copying over "
-          "config.example.ini...".format(config_ini_path))
-    copyfile('config.example.ini', config_ini_path)
+def main(argv):
+    """
+    Generate a Django SECRET_KEY and append it as a [secrets] section to
+    a config.ini.
+    """
 
-print("Appending [secrets] section with key to "
-      "`{}'...".format(config_ini_path))
-open(config_ini_path, 'a').write(secrets_section)
+    # If user supplies first argument, use it as the path to the config.ini to
+    # write to, else use 'config.ini' in the current directory
+    if len(argv) - 1 >= 1:
+        config_ini_path = argv[1]
+    else:
+        config_ini_path = 'config.ini'
+    print("Writing secret key to "
+          "ini file `{}'...".format(os.path.abspath(config_ini_path)))
 
-print('SECRET_KEY generation Done!')
+    print('Generating key...')
+    random_bytes = os.urandom(KEY_LEN)
+    # Work through the random data from /dev/urandom byte by byte, converting it to
+    # characters from POPULATION
+    key = ''.join(POPULATION[int(x / 256 * len(POPULATION))] for x in random_bytes)
+    secrets_section = SECRETS_SECTION.format(key)
+
+    if not os.path.exists(config_ini_path):
+        print("`{}' does not exist, so copying over "
+              "config.example.ini...".format(config_ini_path))
+        copyfile('config.example.ini', config_ini_path)
+
+    print("Appending [secrets] section with key to "
+          "`{}'...".format(config_ini_path))
+    open(config_ini_path, 'a').write(secrets_section)
+
+    print('SECRET_KEY generation Done!')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
