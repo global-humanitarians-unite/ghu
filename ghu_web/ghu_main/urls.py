global-humanitarians-unite/ghu

commit 8e4bd63deaee69386769345edb269605e6960270

Changed home page to About and nav

ghu_web/ghu_main/urls.py
@@ -4,8 +4,10 @@ from ghu_main import views
 app_name = 'ghu_main'
 
 urlpatterns = [
-    url('^$', views.home, name='home'),
-    url('^about/$', views.about, name='about'),
-    url('^orgs/$', views.orgs, name='orgs'),
-    url('^forum/$', views.forum, name='forum'),
+    # url('^$', views.home, name='home'),
+    # url('^about/$', views.about, name='about'),
+    # url('^orgs/$', views.orgs, name='orgs'),
+    # url('^forum/$', views.forum, name='forum'),
+
+    url('$', views.about, name='home')
 ]

ghu_web/ghu_global/templates/ghu_global/layout.html
@@ -9,10 +9,10 @@
         {% endblock %}
     </head>
     <body>
-        <div id="authbar">
+        <!-- <div id="authbar">
             <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#login-modal">Login</button>
             <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target="#register-modal">Sign up</button>
-        </div>
+        </div> -->
         <header>
             {% block header %}{% endblock %}
         </header>

ghu_web/ghu_main/templates/ghu_main/base.html
@@ -15,6 +15,7 @@
         <p>Save the world through togetherness</p>
       </div>
     </div>
+    {% comment %}
     <nav class="navbar">
         <div class="container-fluid">
             <ul class="nav navbar-nav">
@@ -24,4 +25,5 @@
             </ul>
         </div>
     </nav>
+    {% endcomment %}
 {% endblock %}

commit d49fbfc6ccb9877cdace5d7c6463f6ece50ff4db

Stop routing every URL to the about view
A typo in 8e4bd63d accidentally made every URL go to the about page, so
make only / go to the about page.

ghu_web/ghu_main/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     # url('^orgs/$', views.orgs, name='orgs'),
     # url('^forum/$', views.forum, name='forum'),
 
-    url('$', views.about, name='home')
+    url('^$', views.about, name='home')
 ]