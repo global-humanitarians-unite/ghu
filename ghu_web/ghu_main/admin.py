e9cf6497431542b4ec1f63e6bbf1409528ce418e
Austin Adams <git@austinjadams.com>
2017-09-09 00:59:19 -0400

    main: Add Page model
    For the About page right now, and other pages later.

--- ghu_web/ghu_main/admin.py
+++ ghu_web/ghu_main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from ghu_web.admin import admin_site
+from .models import Page
 
-# Register your models here.
+@admin.register(Page, site=admin_site)
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
--- ghu_web/ghu_main/models.py
+++ ghu_web/ghu_main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=256)
+    contents = models.TextField()
+
+    def __str__(self):
+        return 'Page "{}": /{}/'.format(self.title, self.slug)
