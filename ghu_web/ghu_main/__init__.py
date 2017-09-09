b65161211492b68cd821d54775d291ad5b241b52
Austin Adams <git@austinjadams.com>
2017-09-09 00:59:19 -0400

    main: Add Page model
    For the About page right now, and other pages later.

---
+++ ghu_web/ghu_main/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'ghu_main.apps.GhuMainConfig'
--- ghu_web/ghu_main/apps.py
+++ ghu_web/ghu_main/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class GhuMainConfig(AppConfig):
     name = 'ghu_main'
+    verbose_name = 'GHU (Main)'
