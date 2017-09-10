0e24bb954f25ac8c9c95450f049622cdc526a3db
Austin Adams <git@austinjadams.com>
2017-09-10 10:31:52 -0400

    main: Store about page in database, make editable
    Store the about page as an instance of the Page model, and make it
    editable with the Admin WYSIWYG.

--- ghu_web/ghu_main/models.py
+++ ghu_web/ghu_main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Page(models.Model):
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True, unique=True)
     title = models.CharField(max_length=256)
     contents = models.TextField()
+    template = models.ForeignKey('PageTemplate', null=True, blank=True)
 
     def __str__(self):
         return 'Page "{}": /{}/'.format(self.title, self.slug)
+
+class PageTemplate(models.Model):
+    name = models.CharField(max_length=256, verbose_name='User-friendly title')
+    template = models.CharField(max_length=256, verbose_name='Template to execute')
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.template)
--- ghu_web/ghu_main/urls.py
+++ ghu_web/ghu_main/urls.py
@@ -4,10 +4,6 @@ from ghu_main import views
 app_name = 'ghu_main'
 
 urlpatterns = [
-    # url('^$', views.home, name='home'),
-    # url('^about/$', views.about, name='about'),
-    # url('^orgs/$', views.orgs, name='orgs'),
-    # url('^forum/$', views.forum, name='forum'),
-
-    url('^$', views.about, name='home')
+    url('^$', views.page, name='home'),
+    url('^(?P<slug>.+)/$', views.page, name='page'),
 ]
