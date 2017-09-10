0cc2cc01d7016996b17cdc1f4aeeebbe9ef860e8
Austin Adams <git@austinjadams.com>
2017-09-10 09:53:38 -0400

    main: Add WYSIWYG for pages
    Use a CKEditor instance when editing pages in the Django admin.

---
+++ ghu_web/ghu_global/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+
+class RichTextWidget(forms.Textarea):
+    """
+    A CKEditor version of forms.Textarea designed for the Django admin.
+    Must be used with RichTextField below for good results.
+    """
+
+    class Media:
+        js = ('ghu_global/bower/ckeditor/ckeditor.js',)
+        # Hide the <label> for CKEditor (WYSIWYG) fields. Otherwise in
+        # the Admin, CKEditor gets all screwed up: the controls (bold,
+        # italic, etc.) are to the right of the label, but the field is
+        # below and it looks awful.
+        css = {
+            'all': ('ghu_global/css/hide-empty-labels.css',),
+        }
+
+    def __init__(self, **kwargs):
+        if 'attrs' not in kwargs:
+            kwargs['attrs'] = {}
+        # When ckeditor.js starts up, it looks for <textarea>s with the
+        # ckeditor class and replaces them, so give our <textarea> the
+        # ckeditor class
+        kwargs['attrs']['class'] = kwargs['attrs'].get('class', '') + ' ckeditor'
+
+        super().__init__(**kwargs)
+
+class RichTextField(forms.CharField):
+    """
+    A form field which has no label (allowing the WYSIWYG to fill the
+    width of the screen) and uses RichTextField to activate CKEditor.
+    """
+
+    def __init__(self, **kwargs):
+        # Make the <label> element empty so hide-empty-labels.css
+        # (pulled in by RichTextWidget above) will hide it. This class
+        # basically exists to do this in a clean way; in a perfect world
+        # you'd just use CharField with RichTextWidget
+        kwargs['label'] = ''
+        if 'widget' not in kwargs:
+            kwargs['widget'] = RichTextWidget()
+
+        super().__init__(**kwargs)
--- ghu_web/ghu_main/admin.py
+++ ghu_web/ghu_main/admin.py
@@ -1,7 +1,18 @@
+from django import forms
 from django.contrib import admin
 from ghu_web.admin import admin_site
+from ghu_global.forms import RichTextField
 from .models import Page
+
+class PageForm(forms.ModelForm):
+    # Use CKEditor and hide the label
+    contents = RichTextField()
+
+    class Meta:
+        model = Page
+        exclude = []
 
 @admin.register(Page, site=admin_site)
 class PageAdmin(admin.ModelAdmin):
+    form = PageForm
     prepopulated_fields = {'slug': ('title',)}
