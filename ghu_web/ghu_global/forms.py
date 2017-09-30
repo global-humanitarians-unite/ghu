e000a6df36277bfdad9452bb0c6d9fd0eeb68007
Austin Adams <git@austinjadams.com>
2017-09-30 16:37:04 -0400

    ghu_main: Page admin: Finish 'Make home page' checkbox
    Add a checkbox to the Page admin which empties the slug field when
    checked.

--- ghu_web/ghu_global/forms.py
+++ ghu_web/ghu_global/forms.py
@@ -45,18 +45,36 @@ class RichTextField(forms.CharField):
 
 class CheckboxBlankerWidget(forms.CheckboxInput):
     """
+    A checkbox which, when checked, blanks out the prepopulated field
+    named target_field, re-populating it from source_field when
+    necessary.
     """
+
+    def __init__(self, target_field, source_field, **kwargs):
+        if 'attrs' not in kwargs:
+            kwargs['attrs'] = {}
+        # XXX Don't hardcode the `id_' prefix here. Instead, use
+        #     Field.auto_id somehow
+        kwargs['attrs']['data-blanker-target-field'] = '#id_' + target_field
+        kwargs['attrs']['data-blanker-source-field'] = '#id_' + source_field
+
+        super().__init__(**kwargs)
+
     class Media:
-        js = ('ghu_global/js/checkboxBlanker.js',)
+        js = ('admin/js/checkbox_blanker.js',)
 
 class CheckboxBlanker(forms.BooleanField):
     """
-
+    A field which introduces a CheckboxblankerWidget with the provided
+    target_field and source_field (see CheckboxBlankerWidget for
+    details).
     """
-    def __init__(self, **kwargs):
+
+    def __init__(self, target_field, source_field, **kwargs):
         if 'widget' not in kwargs:
-            kwargs['widget'] = CheckboxBlankerWidget()
-        attrs = {'data-blanker-target' : 'id_slug'}
-        kwargs['required'] = False;
+            kwargs['widget'] = CheckboxBlankerWidget(
+                target_field=target_field,
+                source_field=source_field)
+        kwargs['required'] = False
 
         super().__init__(**kwargs)
