116400dda1de87a7c74e36cd2be09264e2bac870
Austin Adams <git@austinjadams.com>
2017-10-01 13:49:01 -0400

    Add toolkits view, template
    Add a view, template for the Toolkits listing

--- ghu_web/ghu_main/views.py
+++ ghu_web/ghu_main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
-from .models import Page, NavbarEntry
+from .models import Page, NavbarEntry, Toolkit
 
 def page(request, slug=None):
     if slug is None:
@@ -17,3 +17,8 @@ def page(request, slug=None):
         template = 'ghu_main/page.html'
     context = {'page': page, 'navbar': NavbarEntry.objects.all()}
     return render(request, template, context)
+
+def toolkits(request):
+    context = {'toolkits': Toolkit.objects.all(),
+               'navbar': NavbarEntry.objects.all()}
+    return render(request, 'ghu_main/toolkits.html', context)
