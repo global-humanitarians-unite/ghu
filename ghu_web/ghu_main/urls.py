86f34f77666e787b40399508ccde101f69592b6b
Sunpil Kim <ezweb28@gmail.com>
2017-10-01 16:05:22 -0400

    Added toolkitpage templete

--- ghu_web/ghu_main/urls.py
+++ ghu_web/ghu_main/urls.py
@@ -6,6 +6,7 @@ app_name = 'ghu_main'
 urlpatterns = [
     url('^$', views.page, name='home'),
     url('^toolkits/$', views.toolkits, name='toolkits'),
-    url('^toolkit/(?P<slug>.+)/$', views.toolkit, name='toolkit'),
+    url('^toolkit/(?P<slug>[a-zA-Z0-9_\-]+)/$', views.toolkit, name='toolkit'),
+    url('^toolkit/(?P<toolkit_slug>[a-zA-Z0-9_\-]+)/(?P<toolkitpage_slug>[a-zA-Z0-9_\-]+)/$', views.toolkitpage, name='toolkitpage'),
     url('^(?P<slug>.+)/$', views.page, name='page'),
 ]
