5106eb949e4f68a89864ab599493d394adee9ffb
Austin Adams <git@austinjadams.com>
2017-10-21 22:25:33 -0400

    deploy: Have ansible script set Django admin password

--- deploy/load_sample_data.sh
+++ deploy/load_sample_data.sh
@@ -2,6 +2,16 @@
 set -e
 set -o pipefail
 
+[[ -z $GHU_ADMIN_PASSWORD ]] && {
+    printf 'error: $GHU_ADMIN_PASSWORD is not set. Set it to the Django admin '`
+          `'password and try again\n' >&2
+    exit 1
+}
+
+# This is sensitive, so don't want to pass this to subsequent commands
+admin_pass=$GHU_ADMIN_PASSWORD
+export GHU_ADMIN_PASSWORD=
+
 [[ -z $GHU_DEPLOY_CONFIG_PATH ]] && {
     printf 'error: $GHU_DEPLOY_CONFIG_PATH is not set. Set it to the path to '`
           `'deploy.config and try again\n' >&2
@@ -12,4 +22,5 @@ set -o pipefail
 
 . "$deploy_root/venv/bin/activate"
 cd "$deploy_root/worktrees/current/ghu_web"
+python manage.py mkadmin <<<"$admin_pass"
 python manage.py loaddata pages
