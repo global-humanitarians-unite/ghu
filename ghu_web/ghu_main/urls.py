e8b26c1d402f2b6f19abd09bbfa7a9dfe55b942c
Sunpil Kim <ezweb28@gmail.com>
2017-10-31 23:50:21 -0400

    change some order in urls.py

--- ghu_web/ghu_main/urls.py
+++ ghu_web/ghu_main/urls.py
@@ -6,14 +6,15 @@ app_name = 'ghu_main'
 
 urlpatterns = [
     url('^$', views.page, name='home'),
+    url('^login/$', auth_views.login, name='login'),
+    url('^logout/$', auth_views.logout, name='logout'),
+    url('^register/$', views.register, name='register'),
     url('^organizations/$', views.organizations, name='organizations'),
     url('^toolkits/$', views.toolkits, name='toolkits'),
     url('^toolkit/(?P<slug>[a-zA-Z0-9_\-]+)/$', views.toolkit, name='toolkit'),
     url('^toolkit/(?P<toolkit_slug>[a-zA-Z0-9_\-]+)/(?P<toolkitpage_slug>[a-zA-Z0-9_\-]+)/$', views.toolkitpage, name='toolkitpage'),
     url('^profiles/(?P<slug>.+)/$', views.org_profile, name='profile'),
     url('^(?P<slug>.+)/$', views.page, name='page'),
-    url('^login/$', auth_views.login, name='login'),
-    url('^logout/$', auth_views.logout, name='logout'),
-    url('^register/$', views.register, name='register'),
+
 
 ]
